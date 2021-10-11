4f6b14d3e9eefe1dce6987f85beee23512e88c91
Donard97 <donardgolaj@gmail.com>
2021-10-11 16:04:46 +0200

    Add route for rockets file and content in it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/nav/Navbar';
+import Missions from './pages/Missions';
 import Profile from './pages/Profile';
+import Rockets from './pages/Rockets';
 
 const App = () => (
   <Router>
     <Navbar />
     <Switch>
+      <Route exact path="/" component={Rockets} />
       <Route exact path="/profile" component={Profile} />
     </Switch>
   </Router>
