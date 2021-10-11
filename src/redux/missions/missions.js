18bfa3d0ccc3906e653fc52c7270d296a578ac37
Donard97 <donardgolaj@gmail.com>
2021-10-11 18:44:16 +0200

    Add missions and dispatch them

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,6 +1,32 @@
+import getMissions from '../../api/index';
+
+const LOAD_MISSIONS = 'space-travelers-hub/missions/LOAD_MISSIONS';
+
 const initialState = [];
+
+export const loadMissions = () => async (dispatch) => {
+  const getResult = await getMissions();
+  const missions = getResult.map((mission) => ({
+    id: mission.mission_id,
+    name: mission.mission_name,
+    description: mission.description,
+  }));
+
+  if (missions) {
+    dispatch({
+      type: LOAD_MISSIONS,
+      payload: missions,
+    });
+  }
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case LOAD_MISSIONS:
+      return {
+        ...state,
+        missions: action.payload,
+      };
     default:
       return state;
   }
