d662a1a01e6b5cf1bf80822a8c30185182cf4a4c
Donard97 <donardgolaj@gmail.com>
2021-10-13 17:21:48 +0200

    Add reserve data to API

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -2,6 +2,7 @@ import API from '../../api/index';
 
 const initialState = [];
 const GET_ROCKETS = 'missionsStore/missions/GET_ROCKETS';
+const RESERVE_ROCKET = 'space_travlers_hub/rockets/RESERVE_ROCKET';
 
 export const getRockets = () => async (dispatch) => {
   const data = await API.getRockets();
@@ -17,11 +18,20 @@ export const getRockets = () => async (dispatch) => {
   }
 };
 
+export const reserveRocket = (id) => ({
+  type: RESERVE_ROCKET,
+  id,
+});
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKETS:
       return [...state, ...action.payload];
-
+    case RESERVE_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: true };
+      });
     default:
       return state;
   }
