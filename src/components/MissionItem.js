eaae4e4fd067c5079601c7e2022cfe1863dc65fc
Donard97 <donardgolaj@gmail.com>
2021-10-13 21:10:11 +0200

    Add event handler for join button

--- src/components/MissionItem.js
+++ src/components/MissionItem.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../redux/missions/missions';
 
-const MissionItem = ({ mission }) => (
-  <tr>
-    <td className="missions_td--bold">{mission.mission_name}</td>
-    <td className="missions_td--desc">{mission.description}</td>
-    <td className="align-center">
-      <span className="badge">NOT A MEMBER</span>
-    </td>
-    <td className="align-center">
-      <button type="button" className="btn-red">
-        Join Mission
-      </button>
-    </td>
-  </tr>
-);
+const MissionItem = ({ mission }) => {
+  const dispatch = useDispatch();
+
+  const join = (id) => {
+    dispatch(joinMission(id));
+  };
+
+  return (
+    <tr key={mission.id}>
+      <td className="missions_td--bold">{mission.name}</td>
+      <td className="missions_td--desc">{mission.description}</td>
+      <td className="align-center">
+        <span className="badge">NOT A MEMBER</span>
+      </td>
+      <td className="align-center">
+        <button type="button" className="btn-red" onClick={() => join(mission.id)}>
+          Join Mission
+        </button>
+      </td>
+    </tr>
+  );
+};
 
 MissionItem.propTypes = {
   mission: PropTypes.shape({
-    mission_id: PropTypes.string.isRequired,
-    mission_name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
   }).isRequired,
 };
