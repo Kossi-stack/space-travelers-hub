9e71caf2aad1b7602882813aec8eb29e1548d8cc
Donard97 <donardgolaj@gmail.com>
2021-10-13 21:09:48 +0200

    Add key for missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,6 +2,7 @@ import API from '../../api/index';
 
 const initialState = [];
 const GET_MISSIONS = 'missionsStore/missions/GET_MISSIONS';
+const JOIN_MISSION = 'space-travelers-hub/missions/JOIN_MISSION';
 
 export const getMissions = () => async (dispatch) => {
   const data = await API.getMissions();
@@ -17,11 +18,20 @@ export const getMissions = () => async (dispatch) => {
   }
 };
 
+export const joinMission = (mission) => ({
+  type: JOIN_MISSION,
+  payload: mission,
+});
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_MISSIONS:
       return [...state, ...action.payload];
-
+    case JOIN_MISSION:
+      return state.map((missions) => {
+        if (missions.id !== action.id) return missions;
+        return { ...missions, reserved: true };
+      });
     default:
       return state;
   }
