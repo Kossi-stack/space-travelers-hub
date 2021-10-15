3bed1d745768db6bdcfecd49b290476ec0d97f0b
Donard97 <donardgolaj@gmail.com>
2021-10-15 02:39:32 +0200

    Add function to add reserved rocket to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import ProfileMission from '../components/ProfileMission';
+import ProfileRocket from '../components/ProfileRocket';
 
 const Profile = () => {
   const missions = useSelector((state) => state.missions.filter((m) => m.reserved === true));
+  const rockets = useSelector((state) => state.rockets.filter((r) => r.reserved === true));
+
   return (
     <main className="container profile">
       <section className="profile__missions">
@@ -17,7 +20,12 @@ const Profile = () => {
       </section>
       <section className="profile__rockets">
         <h1 className="profile__h1">My Rockets</h1>
-        <ul className="profile__ul" />
+        <ul className="profile__ul">
+          {rockets
+            && rockets.map((rocket) => (
+              <ProfileRocket key={rocket.id} rockets={rocket} />
+            ))}
+        </ul>
       </section>
     </main>
   );
